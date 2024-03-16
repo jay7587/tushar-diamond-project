7186c86f1f6835f5ded2722252eddfa936650d84
shubham <shubhamvaghani555@gmail.com>
2024-03-16 18:19:25 +0530

    intial commit

---
+++ loder.js
@@ -0,0 +1,8 @@
+for (let i = 0; i <= 0 ; i++) {
+    console.log(i)
+    setTimeout(function () {
+
+        window.location.href = "main.html";
+
+    }, 2000)
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+function sumValue() {
+    let doller = document.getElementById("number").value
+
+    let inputValue = doller / 100
+
+    outputElement = document.getElementById("output");
+    outputElement.textContent = "Your Diamondas price $: " + inputValue
+}
+
