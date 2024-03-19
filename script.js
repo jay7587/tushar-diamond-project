4188302704b6ab045b1079a7925dd1d0f502149f
shubham <shubhamvaghani555@gmail.com>
2024-03-19 16:17:06 +0530

    fully Dynamic Complete

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-fetch("../../test.json")
-    .then(res => res.json())
-    .then(data => {
-        const outputElement = document.getElementById('output');
-
-        console.log("data", data);
-
-
-        const htmlContent = data.gameChars.map(char => `
-    <a href="./charterpage/${char.name.toLowerCase()}.html" class="col-6 mb-4 text-decoration-none">
-        <div class="card-body rounded-4" style="background-color: ${char.bgColor};">
-            <img class="card-img-top" src="${char.imgUrl}" alt="Card image cap">
-            <p class="card-text text-center fw-bold fs-2 text-black">${char.name}</p>
-        </div>
-    </a>
-`).join('');
-
-
-
-
-        outputElement.innerHTML = htmlContent;
-    })
-    .catch(error => console.error('Error fetching data:', error));
-
-
-
-
-// -----------------------------------------------------
-
 
 function sumValue() {
     let doller = document.getElementById("number").value
