24738ab93f0f386cf33392d50371966d92c9d394
shubham <shubhamvaghani555@gmail.com>
2024-03-20 12:13:27 +0530

    Remove Last Node Pages And Combine In One page

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function sumValue() {
-    let doller = document.getElementById("number").value
-
-    let inputValue = doller / 100
-
-    outputElement = document.getElementById("output");
-    outputElement.textContent = "Your Diamondas price $: " + inputValue
-}
